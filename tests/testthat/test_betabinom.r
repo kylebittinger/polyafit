5ebe13c0bdc6c0d2adf9578b984711f6ba200453
Bittinger <bittingerk@jcf6610fj0.fios-router.home>
2022-11-09 14:54:58 -0500

    Convert expect_that to expect_equal

--- tests/testthat/test_betabinom.r
+++ tests/testthat/test_betabinom.r
@@ -1,27 +1,23 @@
-context("dbetabinom")
-
 test_that("is normalized", {
-  expect_that(sum(dbetabinom(0:10, 10, 4, 16, log=FALSE)), equals(1))
+  expect_equal(sum(dbetabinom(0:10, 10, 4, 16, log=FALSE)), 1)
 })
 
 test_that("is zero when k > n", {
-  expect_that(dbetabinom(7, 5, 3, 8, log=FALSE), equals(0))
+  expect_equal(dbetabinom(7, 5, 3, 8, log=FALSE), 0)
 })
 
 
-context("pbetabinom")
-
 test_that("is unity when k = n", {
-  expect_that(pbetabinom(6, 6, 2, 34, log=FALSE), equals(1))
+  expect_equal(pbetabinom(6, 6, 2, 34, log=FALSE), 1)
 })
 
 test_that("is sum of discrete probabilities", {
-  expect_that(
-    pbetabinom(2, 6, 9, 4, log=FALSE), 
-    equals(sum(dbetabinom(c(0, 1, 2), 6, 9, 4, log=FALSE))))
+  expect_equal(
+    pbetabinom(2, 6, 9, 4, log=FALSE),
+    sum(dbetabinom(c(0, 1, 2), 6, 9, 4, log=FALSE)))
 })
 
 test_that("is less than unity by sum of discrete probabilities", {
-  expect_that(pbetabinom(4, 6, 9, 4, log=FALSE), 
-    equals(1 - sum(dbetabinom(c(5, 6), 6, 9, 4, log=FALSE))))  
+  expect_equal(pbetabinom(4, 6, 9, 4, log=FALSE),
+    1 - sum(dbetabinom(c(5, 6), 6, 9, 4, log=FALSE)))
 })
